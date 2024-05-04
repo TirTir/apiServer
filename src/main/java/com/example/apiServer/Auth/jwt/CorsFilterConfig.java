de283f38f9b932b597d15ed0e1cbad9a503a2e86
TirTir <uj0791navercom@skuniv.ac.kr>
2024-05-04 21:47:01 +0900

    feat. medical data

--- src/main/java/com/example/apiServer/Auth/jwt/CorsFilterConfig.java
+++ src/main/java/com/example/apiServer/Auth/jwt/CorsFilterConfig.java
@@ -1,4 +1,4 @@
-package com.example.apiServer.Auth;
+package com.example.apiServer.Auth.jwt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
