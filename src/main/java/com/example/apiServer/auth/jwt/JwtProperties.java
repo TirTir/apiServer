ddae0c3f3e04f9951778ced4ef5951142c7feb57
TirTir <uj0791navercom@skuniv.ac.kr>
2024-05-13 10:55:59 +0900

    fix. jwt secretkey

--- src/main/java/com/example/apiServer/auth/jwt/JwtProperties.java
+++ src/main/java/com/example/apiServer/auth/jwt/JwtProperties.java
@@ -1,15 +1,15 @@
 package com.example.apiServer.auth.jwt;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
-@Getter @Setter
+@Data
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     private String header;
-    private String issure;
+
+    @Value("${jwt.secret}")
     private String secret;
 }
