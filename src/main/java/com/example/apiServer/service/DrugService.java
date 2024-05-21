bba9f19197ae0c064c2069ed428fcc494347aabf
TirTir <uj0791navercom@skuniv.ac.kr>
2024-05-21 14:13:54 +0900

    fix. error

--- src/main/java/com/example/apiServer/service/DrugService.java
+++ src/main/java/com/example/apiServer/service/DrugService.java
@@ -3,10 +3,12 @@ package com.example.apiServer.service;
 import com.example.apiServer.entity.Drug;
 import com.example.apiServer.repository.DrugRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Service
 public class DrugService {
     @Autowired
     private DrugRepository drugRepository;
